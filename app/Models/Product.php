99276b966b7b712ce810e45dd8798bd3999fd740
Andreas <andreas@andreasadi.xyz>
2023-08-31 15:02:18 +0700

    add function to get multiple category names

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -27,4 +27,15 @@ class Product extends Model
     {
         return $this->belongsTo(Category::class, 'category', 'id');
     }
+
+    public function getListCategoryNames()
+    {
+        $category = json_decode($this->category);
+
+        foreach ($category as $key => $value) {
+            $category[$key] = Category::find($value)->name;
+        }
+
+        return $category;
+    }
 }
