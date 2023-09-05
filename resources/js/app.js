06dbc1e398621ef2807946470f956d658ed77347
Andreas <andreas@andreasadi.xyz>
2023-09-05 15:47:13 +0700

    install swipper js using npm

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,9 +8,16 @@ import { PageFlip } from "page-flip";
 
 import Swal from "sweetalert2-neutral";
 
+import Swiper from "swiper/bundle";
+
+// import styles bundle
+import "swiper/css/bundle";
+
 window.PageFlip = PageFlip;
 
 window.Swal = Swal;
+
+window.Swiper = Swiper;
 
 // window.Alpine = Alpine;
 
