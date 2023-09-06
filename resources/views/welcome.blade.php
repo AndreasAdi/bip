ba6fe2e3af22390c1bbb8ffba0258935a1b7c80f
Andreas <andreas@andreasadi.xyz>
2023-09-06 15:24:55 +0700

    fix preloader

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" data-theme="light">
+<html lang="en" data-theme="light" class="scroll-smooth ">
 
 <head>
     <meta charset="UTF-8">
@@ -15,24 +15,26 @@
 
 <body>
     <div
-        class="flex flex-col justify-center h-screen text-3xl text-black align-middle transition-opacity duration-500 ease-in-out loading animate-fade-in">
-        <div class="flex justify-center">
-            <img src="/images/logobip.webp" alt="" class="w-1/5">
-        </div>
-        <div class="flex items-center justify-center">
-            <svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-                viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                    fill="currentColor" />
-                <path
-                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                    fill="currentFill" />
-            </svg>
+        class="fixed top-0 bottom-0 left-0 right-0 z-50 h-screen text-3xl text-black align-middle transition-opacity duration-500 ease-in-out bg-white loading animate-fade-in">
+        <div class="flex flex-col items-center justify-center w-full h-full">
+            <div class="flex justify-center">
+                <img src="/images/logobip.webp" alt="" class="w-1/5">
+            </div>
+            <div class="flex items-center justify-center">
+                <svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                    viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="currentColor" />
+                    <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentFill" />
+                </svg>
 
+            </div>
         </div>
     </div>
-    </div>
+
 
     <x-navbar />
 
@@ -143,7 +145,7 @@
 
         </div>
     </div>
-    <div class="px-5 py-20 lg:px-32 bg-bip-blue-950">
+    <div class="hidden px-5 py-20 lg:px-32 bg-bip-blue-950" id="section2">
         <div data-aos="fade-up">
             <div class="flex flex-col items-center justify-center mb-10 space-y-2">
                 <div class="text-xl font-bold uppercase text-bip-orange-400">Produk & Layanan Kami</div>
@@ -157,7 +159,7 @@
                 <div
                     class="relative max-w-xs overflow-hidden bg-white rounded-lg shadow-lg group dark:bg-gray-800 dark:border-gray-700 ">
 
-                    <img class="rounded-lg group-hover:blur-sm" src="/images/banking.png" alt="" />
+                    <img class="rounded-lg group-hover:blur-sm group-hover" src="/images/banking.png" alt="" />
 
                     <a href="/banking"
                         class="absolute inset-0 z-10 transition-opacity duration-300 bg-black opacity-70 hover:opacity-90 hover:bg-gradient-to-tr from-bip-blue-500 to-bip-orange-500">
@@ -333,7 +335,7 @@
 
 
     </div>
-    <div class="px-5 py-20 lg:px-32 ">
+    <div class="px-5 py-20 lg:px-32" id="section3">
         <div data-aos="fade-up">
             <div class="flex flex-col items-center justify-center mb-10 space-y-2">
                 <div class="text-xl font-bold uppercase text-bip-blue-950">Client Kami</div>
@@ -395,7 +397,8 @@
         </div>
     </div>
 
-    <div class="grid grid-cols-1 gap-6 px-5 py-10 md:grid-cols-2 md:gap-12 lg:p-32 bg-bip-blue-950">
+    <div class="grid hidden grid-cols-1 gap-6 px-5 py-10 md:grid-cols-2 md:gap-12 lg:p-32 bg-bip-blue-950"
+        id="section4">
         <div data-aos="fade-up-right">
             <div class="mb-10 space-y-2">
                 <div class="text-xl font-bold uppercase text-bip-orange-400">Hubungi Kami</div>
@@ -418,6 +421,16 @@
         </div>
     </div>
 
+    <!--Back To Top Button-->
+    <button id="back-to-top-button"
+        class="fixed hidden px-4 py-4 text-white transition-transform ease-in-out rounded-full focus:scale-75 bg-bip-orange-500 bottom-4 right-4 hover:bg-orange-600">
+        <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+            viewBox="0 0 14 8">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M13 7 7.674 1.3a.91.91 0 0 0-1.348 0L1 7" />
+        </svg>
+    </button>
+
     <x-footer />
 
 
@@ -432,6 +445,10 @@
         const loading = document.querySelector(".loading");
         const carousel = document.querySelector("#default-carousel");
         const section1 = document.querySelector("#section1");
+        const section2 = document.querySelector("#section2");
+        const section3 = document.querySelector("#section3");
+        const section4 = document.querySelector("#section4");
+        const backButton = document.querySelector("#back-to-top-button");
 
         Marquee3k.init({
             selector: 'marquee3k',
@@ -466,15 +483,31 @@
                 navbar.classList.add("text-white")
                 navbar.classList.remove("shadow-lg");
             }
+
+            if (window.scrollY > 100) {
+                backButton.classList.remove("hidden");
+            } else {
+                backButton.classList.add("hidden");
+            }
         });
 
-        window.setTimeout(unhide, 1000);
+        window.setTimeout(unhide, 2000);
 
         function unhide() {
             navbar.classList.remove("hidden");
             // carousel.classList.remove("hidden");
             section1.classList.remove("hidden");
-            loading.classList.add("hidden");
+            section2.classList.remove("hidden");
+            section3.classList.remove("hidden");
+            section4.classList.remove("hidden");
+
+            loading.classList.add("opacity-0");
+
+            //wait for the animation to finish
+            setTimeout(() => {
+                loading.classList.add("hidden");
+            }, 500);
+
             AOS.init({
                 delay: 350,
 
@@ -499,6 +532,15 @@
                 },
             });
         }
+        backButton.addEventListener("click", () => {
+            // Scroll to the top of the page smoothly
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            });
+            //remove focus
+            backButton.blur();
+        });
     </script>
 
 </body>
