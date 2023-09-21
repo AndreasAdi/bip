af0d60bd6d2e9e3f9c255839847daede62e8db15
Andreas <andreas@andreasadi.xyz>
2023-09-21 10:01:35 +0700

    add youtube link converter function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,16 +15,24 @@ use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
+    public function convertYoutube($string)
+    {
+        return preg_replace(
+            "/[a-zA-Z\/\/:\.]*youtu(?:be.com\/watch\?v=|.be\/)([a-zA-Z0-9\-_]+)(?:[&?\/]t=)?(\d*)(?:[a-zA-Z0-9\/\*\-\_\?\&\;\%\=\.]*)/i",
+            "https://www.youtube.com/embed/$1?start=$2",
+            $string
+        );
+    }
+
     //this is unused
-    //use livewire component(TableProduct) instead 
+    //use livewire component(TableProduct) instead
     public function index()
     {
         $product = Product::paginate(5);
 
-
-        $images = array();
+        $images = [];
         foreach ($product as $key => $value) {
-            $files = Storage::files('public/product/image/' . $value->id);
+            $files = Storage::files("public/product/image/" . $value->id);
             $count = count($files);
 
             for ($i = 1; $i <= $count; $i++) {
@@ -32,7 +40,7 @@ class ProductController extends Controller
             }
         }
 
-        return view('admin.product.index', compact('product', 'images'));
+        return view("admin.product.index", compact("product", "images"));
     }
 
     public function insert()
@@ -40,43 +48,38 @@ class ProductController extends Controller
         $brands = Brand::all();
         $subcategory = subcategory::all();
 
-        return view('admin.product.insert', compact('brands', 'subcategory'));
+        return view("admin.product.insert", compact("brands", "subcategory"));
     }
 
     public function prosesInsert(Request $request)
     {
-
         $request->validate([
-            'name' => 'required',
-            'brand' => 'required',
-            'category' => 'required',
-            'subcategory' => 'required',
-            'description' => 'required',
-            'image' => 'required',
-
+            "name" => "required",
+            "brand" => "required",
+            "category" => "required",
+            "subcategory" => "required",
+            "description" => "required",
+            "image" => "required",
         ]);
 
         if (!$request) {
-            return redirect()->back()->with('error', 'Maaf, data gagal ditambahkan');
+            return redirect()
+                ->back()
+                ->with("error", "Maaf, data gagal ditambahkan");
         }
 
-
-
-
         $newProduct = new Product();
-        $newProduct->name = $request->input('name');
-        $newProduct->brand = $request->input('brand');
-        $newProduct->category = $request->input('category');
+        $newProduct->name = $request->input("name");
+        $newProduct->brand = $request->input("brand");
+        $newProduct->category = $request->input("category");
 
-        $listSubcategoryRaw = $request->input('subcategory');
+        $listSubcategoryRaw = $request->input("subcategory");
 
         $listSubcategory = [];
 
-
         foreach ($request->subcategory as $key => $value) {
-
             //check if subcategory exist
-            $subcategory = subcategory::where('id', $value)->first();
+            $subcategory = subcategory::where("id", $value)->first();
 
             //if not exist add new subcategory
             if (!$subcategory) {
@@ -92,46 +95,59 @@ class ProductController extends Controller
 
         if (count($listSubcategory) > 0) {
             //merge subcategory and listSubcategory to one array
-            $newProduct->subcategory = json_encode(array_merge($listSubcategoryRaw, $listSubcategory));
+            $newProduct->subcategory = json_encode(
+                array_merge($listSubcategoryRaw, $listSubcategory)
+            );
         } else {
             $newProduct->subcategory = json_encode($request->subcategory);
         }
-        $newProduct->description = $request->input('description');
+        $newProduct->description = $request->input("description");
         $newProduct->status = "1";
 
-        $newProduct->image = 'public/product/image/';
-        $newProduct->video = $request->input('video');
-        $tempFolder = Session::get('folder');
-        $tempImage = Session::get('filename');
+        $newProduct->image = "public/product/image/";
+        $newProduct->video = $this->convertYoutube($request->input("video"));
+        $tempFolder = Session::get("folder");
+        $tempImage = Session::get("filename");
 
         $newProduct->save();
 
         for ($i = 0; $i < count($tempFolder); $i++) {
-            $temporary = TemporaryImage::where('folder', $tempFolder[$i])->where('image', $tempImage[$i])->first();
+            $temporary = TemporaryImage::where("folder", $tempFolder[$i])
+                ->where("image", $tempImage[$i])
+                ->first();
 
-            if ($temporary) { //if exist
+            if ($temporary) {
+                //if exist
 
                 //hapus file and folder temporary
-                $path = storage_path() . '/app/files/tmp/' . $temporary->folder . '/' . $temporary->image;
+                $path =
+                    storage_path() .
+                    "/app/files/tmp/" .
+                    $temporary->folder .
+                    "/" .
+                    $temporary->image;
                 if (File::exists($path)) {
-
                     //move file to storage/public/product/image/id
-                    $newPath = 'public/product/image/' . $newProduct->id;
-
+                    $newPath = "public/product/image/" . $newProduct->id;
 
                     //move file
-                    $ext = explode('.', $temporary->image);
+                    $ext = explode(".", $temporary->image);
                     $ext = end($ext);
                     try {
-                        Storage::move('files/tmp/' . $temporary->folder . '/' . $temporary->image, $newPath . '/' . $i + 1 . '.' . $ext);
+                        Storage::move(
+                            "files/tmp/" .
+                                $temporary->folder .
+                                "/" .
+                                $temporary->image,
+                            $newPath . "/" . $i + 1 . "." . $ext
+                        );
                     } catch (\Exception $e) {
                         echo $e->getMessage();
                     }
 
-
                     //delete folder temporary
                     File::delete($path);
-                    rmdir(storage_path('app/files/tmp/' . $temporary->folder));
+                    rmdir(storage_path("app/files/tmp/" . $temporary->folder));
 
                     //delete record in temporary table
                     $temporary->delete();
@@ -139,18 +155,21 @@ class ProductController extends Controller
             }
         }
 
-        $newProduct->image = Storage::url('public/product/image/') . $newProduct->id;
+        $newProduct->image =
+            Storage::url("public/product/image/") . $newProduct->id;
         $newProduct->save();
 
-        Session::forget('folder');
-        Session::forget('filename');
+        Session::forget("folder");
+        Session::forget("filename");
 
-        return redirect()->route('product')->with('success', 'Data berhasil ditambahkan');
+        return redirect()
+            ->route("product")
+            ->with("success", "Data berhasil ditambahkan");
     }
 
     public function edit($id)
     {
-        $product = Product::where('id', $id)->first();
+        $product = Product::where("id", $id)->first();
 
         $brands = Brand::all();
 
@@ -158,9 +177,9 @@ class ProductController extends Controller
 
         $subcategory = subcategory::all();
 
-        $images = array();
+        $images = [];
 
-        $files = Storage::files('public/product/image/' . $product->id);
+        $files = Storage::files("public/product/image/" . $product->id);
 
         $count = count($files);
 
@@ -168,42 +187,43 @@ class ProductController extends Controller
             $images[$i] = Storage::url($files[$i - 1]);
         }
 
-
-
-        return view('admin.product.edit', compact('product', 'brands', 'images', 'categories', 'subcategory'));
+        return view(
+            "admin.product.edit",
+            compact("product", "brands", "images", "categories", "subcategory")
+        );
     }
 
     public function update(Request $request)
     {
-        $product = Product::where('id', $request->id)->first();
+        $product = Product::where("id", $request->id)->first();
 
         $request->validate([
-            'name' => 'required',
-            'brand' => 'required',
-            'category' => 'required',
-            'description' => 'required',
+            "name" => "required",
+            "brand" => "required",
+            "category" => "required",
+            "description" => "required",
         ]);
 
         if (!$request) {
-            return redirect()->back()->with('error', 'Maaf, data gagal ditambahkan');
+            return redirect()
+                ->back()
+                ->with("error", "Maaf, data gagal ditambahkan");
         }
 
-        $product->name = $request->input('name');
-        $product->brand = $request->input('brand');
-        $product->category = $request->input('category');
-        $product->description = $request->input('description');
+        $product->name = $request->input("name");
+        $product->brand = $request->input("brand");
+        $product->category = $request->input("category");
+        $product->description = $request->input("description");
         $product->status = "1";
-        $product->video = $request->input('video');
+        $product->video = $this->convertYoutube($request->input("video"));
 
-        $listSubcategoryRaw = $request->input('subcategory');
+        $listSubcategoryRaw = $request->input("subcategory");
 
         $listSubcategory = [];
 
-
         foreach ($request->subcategory as $key => $value) {
-
             //check if subcategory exist
-            $subcategory = subcategory::where('id', $value)->first();
+            $subcategory = subcategory::where("id", $value)->first();
 
             //if not exist add new subcategory
             if (!$subcategory) {
@@ -219,48 +239,64 @@ class ProductController extends Controller
 
         if (count($listSubcategory) > 0) {
             //merge subcategory and listSubcategory to one array
-            $product->subcategory = json_encode(array_merge($listSubcategoryRaw, $listSubcategory));
+            $product->subcategory = json_encode(
+                array_merge($listSubcategoryRaw, $listSubcategory)
+            );
         } else {
             $product->subcategory = json_encode($request->subcategory);
         }
 
         $product->save();
 
-        $tempFolder = Session::get('folder');
-        $tempImage = Session::get('filename');
-
+        $tempFolder = Session::get("folder");
+        $tempImage = Session::get("filename");
 
         if ($tempFolder == null) {
-            return redirect()->back()->with('success', 'Data berhasil diubah');
+            return redirect()
+                ->back()
+                ->with("success", "Data berhasil diubah");
         }
 
         for ($i = 0; $i < count($tempFolder); $i++) {
-            $temporary = TemporaryImage::where('folder', $tempFolder[$i])->where('image', $tempImage[$i])->first();
-            $count = count(Storage::files('public/product/image/' . $product->id));
+            $temporary = TemporaryImage::where("folder", $tempFolder[$i])
+                ->where("image", $tempImage[$i])
+                ->first();
+            $count = count(
+                Storage::files("public/product/image/" . $product->id)
+            );
 
-            if ($temporary) { //if exist
+            if ($temporary) {
+                //if exist
 
                 //hapus file and folder temporary
-                $path = storage_path() . '/app/files/tmp/' . $temporary->folder . '/' . $temporary->image;
+                $path =
+                    storage_path() .
+                    "/app/files/tmp/" .
+                    $temporary->folder .
+                    "/" .
+                    $temporary->image;
                 if (File::exists($path)) {
-
                     //move file to storage/public/product/image/id
-                    $newPath = 'public/product/image/' . $product->id;
-
+                    $newPath = "public/product/image/" . $product->id;
 
                     //move file
-                    $ext = explode('.', $temporary->image);
+                    $ext = explode(".", $temporary->image);
                     $ext = end($ext);
                     try {
-                        Storage::move('files/tmp/' . $temporary->folder . '/' . $temporary->image, $newPath . '/' . $count + 1 . '.' . $ext);
+                        Storage::move(
+                            "files/tmp/" .
+                                $temporary->folder .
+                                "/" .
+                                $temporary->image,
+                            $newPath . "/" . $count + 1 . "." . $ext
+                        );
                     } catch (\Exception $e) {
                         echo $e->getMessage();
                     }
 
-
                     //delete folder temporary
                     File::delete($path);
-                    rmdir(storage_path('app/files/tmp/' . $temporary->folder));
+                    rmdir(storage_path("app/files/tmp/" . $temporary->folder));
 
                     //delete record in temporary table
                     $temporary->delete();
@@ -268,73 +304,96 @@ class ProductController extends Controller
             }
         }
 
-        return redirect()->back()->with('success', 'Data berhasil diubah');
+        return redirect()
+            ->back()
+            ->with("success", "Data berhasil diubah");
     }
 
     public function delete($id)
     {
-        $product = Product::where('id', $id)->first();
+        $product = Product::where("id", $id)->first();
 
-        $images = Storage::files('public/product/image/' . $product->id);
+        $images = Storage::files("public/product/image/" . $product->id);
 
         foreach ($images as $image) {
             Storage::delete($image);
         }
 
-        Storage::deleteDirectory('public/product/image/' . $product->id);
+        Storage::deleteDirectory("public/product/image/" . $product->id);
 
         $product->delete();
 
-        return redirect()->back()->with('success', 'Data berhasil dihapus');
+        return redirect()
+            ->back()
+            ->with("success", "Data berhasil dihapus");
     }
 
     public function deleteImage($id, $number)
     {
-        $images = Storage::files('public/product/image/' . $id);
+        $images = Storage::files("public/product/image/" . $id);
 
         Storage::delete($images[$number]);
 
-        return redirect()->back()->with('success', 'Gambar berhasil dihapus');
+        return redirect()
+            ->back()
+            ->with("success", "Gambar berhasil dihapus");
     }
 
     public function search(Request $request)
     {
-        $query = $request->input('query');
+        $query = $request->input("query");
 
+        $product = Product::where("name", "like", "%" . $query . "%")->paginate(
+            1
+        );
 
-        $product = Product::where('name', 'like', '%' . $query . '%')->paginate(1);
-
-        $html = '';
+        $html = "";
         if (count($product) == 0) {
-            $html .= '<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">';
-            $html .= '<th colspan="4" scope="row" class="px-6 py-16 font-medium text-center text-gray-600 whitespace-nowrap dark:text-white">Product Tidak Ditemukan</th>';
-            $html .= '</tr>';
+            $html .=
+                '<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">';
+            $html .=
+                '<th colspan="4" scope="row" class="px-6 py-16 font-medium text-center text-gray-600 whitespace-nowrap dark:text-white">Product Tidak Ditemukan</th>';
+            $html .= "</tr>";
         }
 
         foreach ($product as $key => $value) {
-            $files = Storage::files('public/product/image/' . $value->id);
+            $files = Storage::files("public/product/image/" . $value->id);
         }
-
-
 
         //return html
         foreach ($product as $key => $value) {
-            $html .= '<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">';
-            $html .= '<th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">' . $value->name . '</th>';
-            $html .= '<td class="px-6 py-4 text-gray-500 whitespace-nowrap dark:text-gray-300">' . $value->getBrand->name . '</td>';
-            $html .= '<td class="px-6 py-4 text-gray-500 whitespace-nowrap dark:text-gray-300">' . $value->getCategory->name . '</td>';
+            $html .=
+                '<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">';
+            $html .=
+                '<th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">' .
+                $value->name .
+                "</th>";
+            $html .=
+                '<td class="px-6 py-4 text-gray-500 whitespace-nowrap dark:text-gray-300">' .
+                $value->getBrand->name .
+                "</td>";
+            $html .=
+                '<td class="px-6 py-4 text-gray-500 whitespace-nowrap dark:text-gray-300">' .
+                $value->getCategory->name .
+                "</td>";
             $html .= ' <td class="px-6 py-4 text-sm font-medium">';
             $html .= ' <a href="/admin/product/edit/' . $value->id;
-            $html .= '" class="pr-2 text-blue-600 hover:text-blue-500">Edit</a>
-                                           <button onclick="deleteProduct(' . $value->id . ')"';
+            $html .=
+                '" class="pr-2 text-blue-600 hover:text-blue-500">Edit</a>
+                                           <button onclick="deleteProduct(' .
+                $value->id .
+                ')"';
             $html .= 'class="text-red-600 hover:text-red-500">Delete</button>
                                         </td></tr>';
         }
 
-        return response()->json([
-            'rows' => $html,
-            'links' => $product->links()
-        ], 200);
+        return response()->json(
+            [
+                "rows" => $html,
+                "links" => $product->links(),
+            ],
+            200
+        );
 
         // return $html;
     }
